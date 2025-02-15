9c2f1cc0e8f52b151e3b2c1cc8b1ccda7c9aad4f
Hammad Allauddin <hammadallauddin@gmail.com>
2025-02-15 09:04:42 +0500

    minor changes

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"log"
+
+	"github.com/hammadallauddin/identity-service/pkg/config"
 )
 
 func main() {
+	if err := config.Initialize(); err != nil {
+		log.Fatalf("coult not initialize config: %v", err)
+	}
+
 	fmt.Println("Starting the service...")
 }
